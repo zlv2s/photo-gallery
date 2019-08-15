a183641ba600d3147efa1316e7b2b215e72e5ab2
zlv2s <706809256@qq.com>
2019-08-15 23:13:48 +0800

    add loading

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,19 @@ import React from 'react'
 import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar'
 import ImageList from './ImageList'
+import Loading from './Loading'
 
 
 class App extends React.Component {
-  state = { images: [] }
+  state = { images: [], loading: false }
   fn = async (data) => {
+    this.setState({ loading: true })
     const res = await unsplash.get('/search/photos', {
       params: {
         query: data
       }
     })
-    this.setState({ images: res.data.results })
+    this.setState({ images: res.data.results, loading: false })
   }
 
   render() {
@@ -20,7 +22,9 @@ class App extends React.Component {
       <div className="ui container">
         <SearchBar callback={this.fn} />
         <h3>Found total {this.state.images.length} images</h3>
-        <ImageList images={this.state.images} />
+        {
+          this.state.loading ? <Loading /> : <ImageList images={this.state.images} />
+        }
       </div>
     )
   }
